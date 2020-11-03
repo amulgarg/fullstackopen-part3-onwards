4b61a82718dfb9d48013bbb089b57d831253d3a2
Amul Garg <amulg2012@gmail.com>
2020-11-03 12:26:37 +0530

    part 3 - completed exercise 3.1

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+
+const persons = [
+    {
+      "name": "Arto Hellas",
+      "number": "040-123456",
+      "id": 1
+    },
+    {
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    {
+      "name": "Dan Abramov",
+      "number": "12-43-234345",
+      "id": 3
+    },
+    {
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122",
+      "id": 4
+    },
+    {
+      "name": "amul",
+      "number": "929499",
+      "id": 5
+    },
+    {
+      "name": "harshit",
+      "number": "2277722",
+      "id": 6
+    },
+    {
+      "name": "dhdehd",
+      "number": "118818",
+      "id": 7
+    },
+    {
+      "name": "feeu8u",
+      "number": "18818",
+      "id": 8
+    },
+    {
+      "name": "dedeuf8Updated",
+      "number": "1777171",
+      "id": 9
+    }
+];
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+  })
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
